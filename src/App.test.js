b0da5b2f81aca56c9dee5908a4fdc81d4af3c3eb
AranzalesO <juanjoaranzales@gmail.com>
2022-09-23 20:42:57 -0500

    First test

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,10 @@
 import { render, screen } from '@testing-library/react';
-
-import React from 'react';
-
 import App from './App';
 
-let test;
-let expect;
-
-test('renders learn react link', () => {
-	render(<App />);
-	const linkElement = screen.getByText(/learn react/i);
-	expect(linkElement).toBeInTheDocument();
+test('Renders user info', () => {
+    // eslint-disable-next-line react/react-in-jsx-scope
+    render( < App / > );
+    const linkElement = screen.getByText(/Loading.../i);
+    // eslint-disable-next-line no-undef
+    expect(linkElement).toBeInTheDocument();
 });
