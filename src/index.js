27544d728be0a6f95345b5317c9e53704337ec66
Bakkym <jcamilito6bh2002@gmail.com>
2022-09-23 19:21:45 -0500

    check code pipeline workflow

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Auth0Provider } from "@auth0/auth0-react";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import { Auth0Provider } from "@auth0/auth0-react"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <React.StrictMode>
-    <Auth0Provider domain='dev-0wsz17nn.us.auth0.com' clientId='QdKG0YHD9GgCDqoQSEiJWYyTEo1r9WOA' redirectUri={window.location.origin}>
-      <App />
-    </Auth0Provider>
-  </React.StrictMode>
-);
+	<React.StrictMode>
+		<Auth0Provider
+			domain="dev-0wsz17nn.us.auth0.com"
+			clientId="QdKG0YHD9GgCDqoQSEiJWYyTEo1r9WOA"
+			redirectUri={window.location.origin}
+		>
+			<App />
+		</Auth0Provider>
+	</React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
