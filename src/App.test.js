41b747674417fd2b3e12b0d5db685cff53c9979f
AranzalesO <juanjoaranzales@gmail.com>
2022-09-25 15:21:51 -0500

    New commit

--- src/App.test.js
+++ src/App.test.js
@@ -4,14 +4,7 @@ import React from 'react';
 import App from './App';
 
 test('Renders user info', () => {
-<<<<<<< HEAD
-    render( < App / > );
-    const linkElement = screen.getByText(/Loading.../i);
-    expect(linkElement).toBeInTheDocument();
-});
-=======
 	render(<App />);
 	const linkElement = screen.getByText(/Loading.../i);
 	expect(linkElement).toBeInTheDocument();
 });
->>>>>>> 2bebd15e64915bdefba1fa6c0ac2e71192e9f192
