644f3962626031daf1b15d0aeeb97372b9a645f8
AranzalesO <juanjoaranzales@gmail.com>
2022-10-24 12:49:48 -0500

    Added images to readme locally

--- frontend/src/components/Loading/Loading.js
+++ frontend/src/components/Loading/Loading.js
@@ -3,6 +3,7 @@ import "./Loading.css";
 
 export default function Loading() {
   return (
+    /* Animation loader */
     <div className="loading-container">
       <div class="loading">
         <div class="circle"></div>
--- frontend/src/components/Pagination/Pagination.js
+++ frontend/src/components/Pagination/Pagination.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Pagination, Grid } from "@nextui-org/react";
+import './Pagination.css'
 
-function Pagination() {
+export default function Pagination_page({setCurrentPage, currentPage, total}) {
+
   return (
-    <div>Pagination</div>
-  )
+    <div className="pagination-container">
+      <Grid.Container gap={2} justify={'center'} style={{width: '100%'}}>
+        <Grid xs={12} style={{width: '100%'}} justify={'center'}>
+          <Pagination loop shadow color={'primary'} total={total}  page={currentPage} onChange={setCurrentPage}  />
+        </Grid>
+        
+      </Grid.Container>
+    </div>
+  );
 }
-
-export default Pagination
