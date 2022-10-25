fe8e75d701d85a0bf67d63706b26d7845d6a5b4a
AranzalesO <juanjoaranzales@gmail.com>
2022-10-25 09:44:03 -0500

    Returning username on auth controller, fizing size on big screens for slider

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -4,56 +4,57 @@ import { SECRET } from "../config.js";
 import Role from "../models/Role.js";
 import Wishlist from "../models/Wishlist.js";
 
-export const signUp = async (req, res) => {
-  const { username, email, password, name, lastname, address, phone } = req.body;
+export const signUp = async(req, res) => {
+    const { username, email, password, name, lastname, address, phone } = req.body;
 
-  const newUser = new User({
-    username,
-    email,
-    password: await User.hashPassword(password),
-    name,
-    lastname,
-    address,
-    phone
+    const newUser = new User({
+        username,
+        email,
+        password: await User.hashPassword(password),
+        name,
+        lastname,
+        address,
+        phone
 
-  });
+    });
 
-  const role = await Role.findOne({ name: "user" });
-  newUser.roles = [role._id];
+    const role = await Role.findOne({ name: "user" });
+    newUser.roles = [role._id];
 
-  const savedUser = await newUser.save();
-  console.log(savedUser._id);
+    const savedUser = await newUser.save();
+    console.log(savedUser._id);
 
-  const newWishlist = new Wishlist({
-    "user": savedUser._id
-  })
+    const newWishlist = new Wishlist({
+        "user": savedUser._id
+    })
 
-  await newWishlist.save()
+    await newWishlist.save()
 
-  const token = jwt.sign({ id: savedUser._id }, SECRET, {
-    expiresIn: 86400, // 24 hours
-  });
+    const token = jwt.sign({ id: savedUser._id }, SECRET, {
+        expiresIn: 86400, // 24 hours
+    });
 
-  res.status(200).json({ token });
+    res.status(200).json({ token });
 };
 
 
 
-export const signIn = async (req, res) => {
-  const userFound = await User.findOne({ email: req.body.email }).populate("roles"); // Populate: Traer los datos de la relación
+export const signIn = async(req, res) => {
+    const userFound = await User.findOne({ email: req.body.email }).populate("roles"); // Populate: Traer los datos de la relación
 
-  if (!userFound) return res.status(400).json({ message: 'User not found' })
+    if (!userFound) return res.status(400).json({ message: 'User not found' })
 
-  const matchPassword = await User.comparePassword(req.body.password, userFound.password)
+    const matchPassword = await User.comparePassword(req.body.password, userFound.password)
 
-  if (!matchPassword) return res.status(401).json({ token: null, message: 'Invalid password' })
+    if (!matchPassword) return res.status(401).json({ token: null, message: 'Invalid password' })
 
-  const token = jwt.sign({ id: userFound._id }, SECRET, {
-    expiresIn: 86400 // 24 hours
-  })
+    const token = jwt.sign({ id: userFound._id }, SECRET, {
+        expiresIn: 86400 // 24 hours
+    })
 
-  const userId = userFound._id
-  console.log(userFound)
+    const userId = userFound._id
+    const username = userFound.username
+    console.log(userFound)
 
-  res.json([{ userId }, { token }])
+    res.json([{ userId }, { token }, { username }])
 };
