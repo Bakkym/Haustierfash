7ca7eeac41bf6919e2ba7cee9ecda6c1444e3e2a
Bakkym <jcamilito6bh2002@gmail.com>
2022-10-25 15:50:17 -0500

    fix register backend

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -17,19 +17,15 @@ const userSchema = new Schema(
     },
     name: {
       type: String,
-      required: true,
     },
     lastname: {
       type: String,
-      required: true,
     },
     address: {
       type: String,
-      required: true,
     },
     phone: {
       type: String,
-      required: true,
     },
 
     roles: [
