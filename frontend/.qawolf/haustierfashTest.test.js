46bb91d838093793952d2104a184476e36f4a506
bakkym <jcamilito6bh2002@gmail.com>
2022-11-03 08:27:34 -0500

    update smoke test

--- frontend/.qawolf/haustierfashTest.test.js
+++ frontend/.qawolf/haustierfashTest.test.js
@@ -14,11 +14,12 @@ afterAll(async () => {
   await browser.close();
 });
 
-test("HomeTest", async () => {
+test("haustierfashTest", async () => {
   const page = await context.newPage();
   await page.goto("http://localhost:3000/", { waitUntil: "domcontentloaded" });
-  await page.click("text=HOME");
   await page.click("text=SHOP");
   await page.click('text="ABOUT US"');
   await page.click("text=REGISTER");
+  await page.click("text=HOME");
+  await qawolf.scroll(page, "html", { x: 0, y: 1949 });
 });
